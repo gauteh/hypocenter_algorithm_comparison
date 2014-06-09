9799922925a78b5c6adfbbe88af7d5b18d794869
Gaute Hope <eg@gaute.vetsj.com>
2014-06-09 10:53:08 +0200

    hypomod: wip

---
+++ hypomod.py
@@ -0,0 +1,17 @@
+#! /usr/bin/python
+#
+# author: Gaute Hope <eg@gaute.vetsj.com> / 2014-06-09
+#
+# Class for interfacing with HYPOMOD (forward modelling for HYPOSAT)
+#
+
+import os, sys
+import numpy as np
+import scipy as sc
+
+from subprocess import check_call, check_output
+
+class Hypomod:
+  def __init__ (self, outdir):
+    self.outdir = outdir
+
